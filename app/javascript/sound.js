2f59d71801b055d513b15088b074acf972d656a8
kakehi22222 <kakehi22222@gmail.com>
2022-10-25 22:33:46 +0900

    効果音の追加

--- app/javascript/sound.js
+++ app/javascript/sound.js
@@ -1,17 +1,18 @@
-// function sound() {
+function sound() {
 //     // const music = document.getElementById("big");
 //     // const sound = document.getElementById("sound");
 //     // music.addEventListener('click', function(){
 //     //   sound.play();
 //     //   }); 
-//       const ninjas = document.getElementById("ninjas");
-//       const kenSound = document.getElementById("ken_sound");
-//       ninjas.addEventListener('click', function(){
-//         kenSound.play();
-//         });
+      const ninjas = document.getElementById("ninjas");
+      const kenSound = document.getElementById("ken_sound");
+      ninjas.addEventListener('click', function(){
+        kenSound.play();
+        kenSound.volume = 0.5;
+        });
 //   // const gameSound = document.getElementById("game_sound");
 //   // setTimeout(function(){
 //   //   gameSound.play();
 //   //   }, 1*1000);
-// };
-//   window.addEventListener('load', sound)
+};
+  window.addEventListener('load', sound)
