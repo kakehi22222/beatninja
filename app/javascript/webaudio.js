26b35f67db448ff4caca79154dcba52adeed87fb
kakehi22222 <kakehi22222@gmail.com>
2022-10-28 16:13:43 +0900

    css見た目補正

---
+++ app/javascript/webaudio.js
@@ -0,0 +1,20 @@
+loadAndPlay();
+
+let source;
+
+async function loadAndPlay() {
+  const context = new AudioContext();
+
+  const data = await fetch('assets/seisen.mp3');
+  const buffer = await data.arrayBuffer();
+  const decodeBuffer = await context.decodeAudioData(buffer);
+
+  source = context.createBufferSource();
+  source.buffer = decodeBuffer;
+  source.connect(context.destination);
+  source.start(0)
+}
+
+function stop() {
+  source.stop();
+}
