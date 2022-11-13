e6f6dd6dd7e379c25e4fc4d7002e9eceadf67d9c
kakehi22222 <kakehi22222@gmail.com>
2022-11-13 17:08:08 +0900

    タイマー表示

--- app/javascript/fall.js
+++ app/javascript/fall.js
@@ -1,5 +1,20 @@
 function fall() {
 
+  const totalTime = 10000;
+  const oldTime = Date.now();
+  const timerId = setInterval(() => {
+    const currentTime = Date.now();
+    const diff = currentTime - oldTime;
+    const remainMSec = totalTime - diff;
+    const remainSec = Math.ceil(remainMSec/1000);
+    let label = `${remainSec}`
+    if (remainMSec <= 0) {
+      clearInterval(timerId);
+      label = '0';
+    }
+    document.querySelector('#last_time_s').innerHTML = label
+  },1000);
+
   // スピードA団
   document.querySelector(`#ninjas_a`).animate(
     [
@@ -150,10 +165,10 @@ function fall() {
     ninjas.setAttribute("style", "display:none;");
   }, 10*1000);
 
-  // setTimeout(function(){
-  //   const gameScreen = document.getElementById("game_image");
-  //   gameScreen.setAttribute("style","display:none;");
-  // }, 10*1000);
+  setTimeout(function(){
+    const gameScreen = document.getElementById("game_image");
+    gameScreen.setAttribute("style","display:none;");
+  }, 10*1000);
 
   
   
