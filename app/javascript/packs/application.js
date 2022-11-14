5fb392bd0fff1a1427be2d03004644381813a9ee
kakehi22222 <kakehi22222@gmail.com>
2022-11-14 11:03:29 +0900

    js登録

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../fall")
 require("../sound")
+require("../webaudio")
 //= require audiojs
 
 // Uncomment to copy all static images under ../images to the output folder and reference
