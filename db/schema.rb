610ecd81fc7bc20b12b1e97854e089805a54f178
kakehi22222 <kakehi22222@gmail.com>
2022-11-27 10:54:08 +0900

    アニメーションの挑戦

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2022_10_15_012457) do
 
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
